5069e2bc8009bada77181e83b4bc4988169dab9c
sprit-maberasu-1140-source <>
2026-05-09 07:24:45 +0000

    Lambda IIFE

---
+++ lambda_iife/main.py
@@ -0,0 +1,8 @@
+celsius_temperature = 23
+
+# The `lambda` function for converting temperature 
+# from `celsius_temperature` variable to fahrenheit temperature 
+fahrenheit_temperature = (___)(___)
+
+# Testing the result
+print(f"{celsius_temperature} degrees Celsius is equal to {fahrenheit_temperature} degrees Fahrenheit")
