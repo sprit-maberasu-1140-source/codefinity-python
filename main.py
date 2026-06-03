6c829ff171142440375cea74f53f48a7de8cfdad
sprit-maberasu-1140-source <>
2026-06-03 12:30:09 +0000

    チャレンジ：グリッドによる空間データの可視化

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
-# Load the world countries dataset from Natural Earth (GeoJSON format)
-world_url = "https://raw.githubusercontent.com/nvkelso/natural-earth-vector/master/geojson/ne_110m_admin_0_countries.geojson"
-world = gpd.read_file(world_url)
 
-# Filter for European countries using the 'CONTINENT' column
-continent = 'Europe'
-europe = world[world['CONTINENT'] == continent]
+def plot_comparison_grid(gdf1, gdf2, attribute, title1, title2):
+    fig, axes = plt.subplots(1, 2, figsize=(14, 6))
+    cmap = 'viridis'
+    
+    vmin = min(float(gdf1[attribute].min()), float(gdf2[attribute].min()))
+    vmax = max(float(gdf1[attribute].max()), float(gdf2[attribute].max()))
+    
+    gdf1.plot(column=attribute, ax=axes[0], legend=True, cmap=cmap, edgecolor='black', vmin=vmin, vmax=vmax)
+    axes[0].set_title(title1)
+    axes[0].set_axis_off()
+    
+    gdf2.plot(column=attribute, ax=axes[1], legend=True, cmap=cmap, edgecolor='black', vmin=vmin, vmax=vmax)
+    axes[1].set_title(title2)
+    axes[1].set_axis_off()
+    
+    plt.tight_layout()
+    plt.show()
 
-# Plot all world countries in light gray
-ax = world.plot(color='lightgray', edgecolor='white', figsize=(10, 6))
+# Example usage with built-in world data
+url = "https://naciscdn.org/naturalearth/110m/cultural/ne_110m_admin_0_countries.zip"
+world = gpd.read_file(url)
 
-# Overlay European countries in a distinct color (not blue or green)
-europe.plot(ax=ax, color='goldenrod', edgecolor='black', label='Europe')
+# Make sure we use the uppercase CONTINENT and POP_EST matching the official dataset
+africa = world[world['CONTINENT'] == 'Africa']
+asia = world[world['CONTINENT'] == 'Asia']
 
-# Add a title and legend
-plt.title("Countries of Europe")
-plt.legend()
-plt.show()
+plot_comparison_grid(africa, asia, 'POP_EST', 'Africa Population', 'Asia Population')
