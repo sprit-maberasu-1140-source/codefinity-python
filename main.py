bf6e460002ef413d78a5eb02300826abfce23651
sprit-maberasu-1140-source <>
2026-06-07 06:20:11 +0000

    チャレンジ：ポリゴンプロット

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-def are_collinear(p1, p2, p3):
-    # Write your code here
-    x1, y1 = p1
-    x2, y2 = p2
-    x3, y3 = p3
-    # Calculate the area of triangle formed by the points
-    area = x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)
-    return area == 0
+import matplotlib.pyplot as plt
 
-# Sample calls
-result1 = are_collinear((0, 0), (1, 1), (2, 2))
-print(result1)
-result2 = are_collinear((0, 0), (1, 2), (2, 1))
-print(result2)
-result3 = are_collinear((1, 2), (2, 4), (3, 6))
-print(result3)
+def plot_polygon(vertices):
+    x_coords = [vertex[0] for vertex in vertices]
+    y_coords = [vertex[1] for vertex in vertices]
+    x_closed = x_coords + [x_coords[0]]
+    y_closed = y_coords + [y_coords[0]]
+    plt.figure()
+    plt.plot(x_closed, y_closed, marker='o')
+    plt.title("Polygon Plot")
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.axis('equal')
+    plt.show()
+
+vertices1 = [(0, 0), (2, 0), (1, 2)]
+plot_polygon(vertices1)
+
+vertices2 = [(1, 1), (4, 1), (4, 4), (1, 4)]
+plot_polygon(vertices2)
