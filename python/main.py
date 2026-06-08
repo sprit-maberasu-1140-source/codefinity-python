dccb5ab0fdebd0c784c65ab54eb8a6efdaed2a97
sprit-maberasu-1140-source <>
2026-06-08 06:23:02 +0000

    チャレンジ：複数要素のアンパック

--- python/main.py
+++ python/main.py
@@ -1,14 +1,12 @@
-def sum_first_last(numbers):
-    if len(numbers) == 1:
-        return numbers[0] * 2
-    else:
-        first, *_, last = numbers
-        return first + last
+def unpack_first_last(lst):
+    first1, first2, *_, last2, last1 = lst
+    result = (first1, first2, last2, last1)
+    return result
 
 
-output1 = sum_first_last((1, 2, 3, 4, 5))
-print(output1)
-output2 = sum_first_last((10, 20))
-print(output2)
-output3 = sum_first_last((7,))
-print(output3)
+res1 = unpack_first_last([1, 2, 3, 4, 5, 6])
+print(res1)
+res2 = unpack_first_last(['a', 'b', 'c', 'd'])
+print(res2)
+res3 = unpack_first_last([10, 20, 30, 40, 50])
+print(res3)
