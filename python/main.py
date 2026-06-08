7cb8e65ed47e1af8583c228ed1641e55ac81fbd8
sprit-maberasu-1140-source <>
2026-06-08 06:16:53 +0000

    チャレンジ：アンパッキングによる辞書のマージ

--- python/main.py
+++ python/main.py
@@ -1,6 +1,11 @@
-def pack_to_tuple(a, b, c):
-    return (a, b, c)
+def merge_dicts(dict1, dict2):
+    merged = {**dict1, **dict2}
+    return merged
 
-values = [10, 20, 30]
-result = pack_to_tuple(*values)
-print(result)
+
+result1 = merge_dicts({'a': 1, 'b': 2}, {'b': 3, 'c': 4})
+print(result1)
+result2 = merge_dicts({}, {'x': 10})
+print(result2)
+result3 = merge_dicts({'foo': 9}, {})
+print(result3)
