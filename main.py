07d3dd1f4e621998994426a7303b5b304a81832d
sprit-maberasu-1140-source <>
2026-06-09 12:37:58 +0000

    チャレンジ：ヒーローをパワーアップ

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-def process_numbers(numbers):
-    def square_and_add_five(x):
-        return x ** 2 + 5
-    result = []
-    for num in numbers:
-        processed = square_and_add_five(num)
-        result.append(processed)
-    return result
+def create_power_up(multiplier):
+    def power_up(base_stat):
+        return base_stat * multiplier
+    return power_up
 
-output = process_numbers([1, 2, 3])
-print(output)
+power_up_double = create_power_up(2)
+power_up_triple = create_power_up(3)
+
+hero_attack = 10
+hero_defense = 5
+
+result_attack = power_up_double(hero_attack)
+result_defense = power_up_triple(hero_defense)
+
+print(result_attack)
+print(result_defense)
