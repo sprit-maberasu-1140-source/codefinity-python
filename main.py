353a6ff0ccd90cd924f7388dc40c124760d647ac
sprit-maberasu-1140-source <>
2026-06-17 13:28:40 +0000

    チャレンジ：交差検証によるモデルの評価

--- main.py
+++ main.py
@@ -1,33 +1,18 @@
 import pandas as pd
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
-from sklearn.compose import make_column_transformer
-from sklearn.pipeline import make_pipeline
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split, cross_val_score
 
-# データの読み込み
-df = pd.read_csv(
-    'https://codefinity-content-media.s3.eu-west-1.amazonaws.com/a65bbc96-309e-4df9-a790-a1eb8c815a1c/penguins.csv'
-)
-
-# 欠損値が2つ以上ある行を削除
-df = df[df.isna().sum(axis=1) < 2]
-
-# 説明変数Xと目的変数yに分割
+df = pd.read_csv('https://codefinity-content-media.s3.eu-west-1.amazonaws.com/a65bbc96-309e-4df9-a790-a1eb8c815a1c/penguins_pipelined.csv')
+# Assign X, y variables (X is already preprocessed and y is already encoded)
 X, y = df.drop('species', axis=1), df['species']
-
-# sex, islandのみOneHotでエンコードし、他の列はそのまま残すColumnTransformerを作成
-ct = make_column_transformer(
-    (OneHotEncoder(), ['sex', 'island']),
-    remainder='passthrough'
-)
-
-# ColumnTransformer → 欠損値補完 → 標準化 の順で実行するパイプラインを構築
-pipe = make_pipeline(
-    ct,
-    SimpleImputer(strategy='most_frequent'),
-    StandardScaler()
-)
-
-# Xを変換してX_transformedに保存
-X_transformed = pipe.fit_transform(X)
-print(X_transformed)
+# Initialize a model
+model = KNeighborsClassifier(n_neighbors=4)
+# Calculate and print the mean of cross validation scores
+scores = cross_val_score(model, X, y, cv=3)
+print('Cross-val score:', scores.mean())
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+# Train a model
+model.fit(X_train, y_train)
+# Print the score using the test set
+print('Train-test score:', model.score(X_test, y_test))
